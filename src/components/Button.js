8d81b310b1ce845dbfcf7b9098f7ee10ce0027be
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-18 10:21:51 -0700

    Pass tests for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  return (
+    <button className={classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger,
+    })} {...props}>
+      {props.children}
+    </button>
+  );
 }
