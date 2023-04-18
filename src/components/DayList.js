43301cdca9749db10ca21371eaba59fd81bd8a50
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-18 12:40:27 -0700

    Add functionality to DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,15 @@ import DayListItem from "./DayListItem";
 const DayList = (props) => {
   return (
     <ul>
-      {props.days.map((day) => (
-        <DayListItem name={day.name} spots={day.spots} message={day.message} />
+      {props.days.map((day, i) => (
+        <DayListItem
+          key={i}
+          name={day.name}
+          selected={props.day === day.name}
+          spots={day.spots}
+          message={day.message}
+          setDay={props.setDay}
+        />
       ))}
     </ul>
   );
