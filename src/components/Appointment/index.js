4c23071e68351f103cf47f3126a0c3b1155ccce1
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-19 10:19:20 -0700

    Create Appointment component with stories

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Appointment = (props) => {
+   return <article className="appointment">{props.time}</article>;
+};
+
+export default Appointment;
