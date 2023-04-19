dca5d7ffee2d933d299a522dc2fda5d01bd0edb4
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-19 11:46:39 -0700

    Render appointments onto application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 const Appointment = (props) => {
+   
    return (
       <article className="appointment">
          <Header time={props.time} />
