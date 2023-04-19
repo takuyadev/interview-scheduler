90fde19da4830c1e20e6b6aa7bbc332088ddb444
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-19 11:24:04 -0700

    Add controlled inputs to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,15 +3,22 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 const Form = (props) => {
-   const [student, setStudent] = useState(props.student || null);
-   const [interviewer, setInterviewer] = useState(props.student || null);
+   const [student, setStudent] = useState(props.student || "");
+   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+   const reset = () => {
+      setStudent("");
+      setInterviewer(null);
+   };
 
    const handleCancel = () => {
       props.onCancel();
+      reset();
    };
 
    const handleSave = () => {
       props.onSave();
+      reset();
    };
 
    const handleInput = (event) => {
@@ -21,11 +28,12 @@ const Form = (props) => {
    return (
       <main className="appointment__card appointment__card--create">
          <section className="appointment__card-left">
-            <form autoComplete="off">
+            <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
                <input
                   className="appointment__create-input text--semi-bold"
                   name="name"
                   type="text"
+                  value={student}
                   placeholder="Enter Student Name"
                   onChange={handleInput}
                />
@@ -33,6 +41,7 @@ const Form = (props) => {
             <InterviewerList
                interviewer={interviewer}
                interviewers={props.interviewers}
+               onChange={setInterviewer}
             />
          </section>
          <section className="appointment__card-right">
