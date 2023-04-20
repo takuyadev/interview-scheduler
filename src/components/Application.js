14221f9eb54845838e0c5ad65a40923d824ba0b6
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-20 15:58:28 -0700

    Finish rendering appointments using API on page

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,51 +1,53 @@
+import "components/Application.scss";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
-   const [days, setDays] = useState([]);
-   const [appointments, setAppointments] = useState(null)
-   const [day, setDay] = useState("Monday");
+   // Setup entire app state for project
+   const [state, setState] = useState({
+      day: "Monday",
+      days: [],
+      appointments: null,
+   });
 
-   useEffect(()=>{
-      axios.get('http://localhost:8001/api/days').then((res)=>{
-         setDays(res.data)
+   // State aliaeses
+   const setDay = (day) => setState({ ...state, day });
+
+   const dailyAppointments = [];
+
+   // On load call for days and appointments to populate with data
+   useEffect(() => {
+      Promise.all([
+         axios.get("http://localhost:8001/api/days"),
+         axios.get("http://localhost:8001/api/appointments")
+      ])
+
+      .then((res)=>{
+         setState(prev => ({...prev, days: res[0].data, appointments: res[1].data}))
       })
-
-      axios.get('http://localhost:8001/api/appointments').then((res)=>{
-         setAppointments(res.data)
-      })
-   },[])
+   }, []);
 
 
+   // Render appointment list based on state.appointments
    const AppointmentList = () => {
-      return Object.values(appointments).map((appointment, i) => (
-         <Appointment key={i} {...appointment} />
-      ));
+      const filterAppointments = getAppointmentsForDay(state, state.day)
+      return filterAppointments.map((appointment, i) => <Appointment key={i} {...appointment} />);
    };
 
    return (
       <main className="layout">
          <section className="sidebar">
-            <img
-               className="sidebar--centered"
-               src="images/logo.png"
-               alt="Interview Scheduler"
-            />
+            <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
             <hr className="sidebar__separator sidebar--centered" />
             <nav className="sidebar__menu">
-               <DayList days={days} day={day} setDay={setDay} />
+               <DayList days={state.days} day={state.day} onChange={setDay} />
             </nav>
-            <img
-               className="sidebar__lhl sidebar--centered"
-               src="images/lhl.png"
-               alt="Lighthouse Labs"
-            />
+            <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
          </section>
-         <section className="schedule">{appointments && <AppointmentList />}</section>
+         <section className="schedule">{state.appointments && <AppointmentList />}</section>
       </main>
    );
 }
